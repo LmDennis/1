131abe9c3ce46a07918f53e012fc47778e5daeb4
devDennis <dennis.bkcell@gmail.com>
2023-10-04 01:54:43 -0300

    aprendendo a utilizar o comando STASH para salvar modificações em memoria cache

--- home.js
+++ home.js
@@ -1,3 +1,3 @@
 function mostraNome(nome){
-    return nome
+    return nome;
 }
--- login.js
+++ login.js
@@ -1,3 +1,9 @@
 //trabalhando no login 
 
 //finalizando meu sistema de login 
+function login (nome, email){
+    return 'Bem vindo';
+}
+
+//seja bem vindo 
+//sistema de login completo 
