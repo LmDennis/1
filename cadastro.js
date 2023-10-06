755a1e139188a889a1285d8c7cf3524629e706f1
devDennis <dennis.bkcell@gmail.com>
2023-10-05 23:30:00 -0300

    adicnionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,10 @@
 function singUp(email ){
     return email;
 }
-
 //pegando um email e cadastrando na plataforma 
+
+function MostraMensagem() {
+    return 'Bem vondo ao sistema'
+}
+
+//adicnionando função de mensagem 
